c16c965779def532747d551e9baeeb54fd328aeb
RamonaEJSpence <ramonaejspence@gmail.com>
2020-05-18 12:11:15 -0400

    navbar now wraps at 425px breakpoint

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,7 +8,7 @@ class Nav extends Component {
     render() {
         return(
             <div className="row no-gutters">
-          <div id="navbar" className="col-12 custom_btn animated slideinright">
+          <div id="navbar" className="col-lg-12 custom_btn animated slideinright">
             <Link to="/" className="btn btn-rabbit back">Home</Link>
             <Link to="/about/" className="btn btn-rabbit back">About</Link>
             <Link to="/work/" className="btn btn-rabbit back">Work</Link>
